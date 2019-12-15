6be57a20f1589971615706451ab85706cd78acfb
Artur <artursfdc@gmail.com>
2019-12-15 20:38:35 +0200

    Teammate generator

---
+++ routes/teammate.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var teams = [];
+teams.push({teamName:'Black', members:'khusnullin-chuiko'});
+teams.push({teamName:'Red', members:'snow-shevchuk'});
+teams.push({teamName:'White', members:'dovhan-skurchak'});
+
+router.get('/', function(req, res, next) {
+    try {
+        let memberLastName = req.headers['name'];
+        let team = teams.find(element => element.members.includes(memberLastName));
+        res.send('Congratulations! You have successfully completed the first task. You are in the "' + team.teamName + '" team. Go ahead and find your teammate. Good luck!');
+    } catch (e) {
+        res.send('Incorrect last name');
+    }
+});
+
+module.exports = router;
