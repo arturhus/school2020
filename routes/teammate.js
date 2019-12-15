054886685937dc618616b1c6236559e081d0b796
Artur <artursfdc@gmail.com>
2019-12-15 20:57:34 +0200

    Teammate generator fix

--- routes/teammate.js
+++ routes/teammate.js
@@ -7,12 +7,14 @@ teams.push({teamName:'Red', members:'snow-shevchuk'});
 teams.push({teamName:'White', members:'dovhan-skurchak'});
 
 router.get('/', function(req, res, next) {
+    let memberLastName = req.headers['lastname'];
+    if(!memberLastName) res.send('Incorrect header. Please try again.');
+
     try {
-        let memberLastName = req.headers['lastName'];
         let team = teams.find(element => element.members.includes(memberLastName));
-        res.send('Congratulations! You have successfully completed the first task.\n\n You are in the "' + team.teamName + '" team. Go ahead and find your teammate. Good luck!');
+        res.send('Congratulations! You have successfully completed the first task. You are in the "' + team.teamName + '" team. Go ahead and find your teammate. Good luck!');
     } catch (e) {
-        res.send('Incorrect last name');
+        res.send('Incorrect Last Name. Please try again.');
     }
 });
 
